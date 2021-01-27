8d9ffa4412ee76ce63a186ce01aa912442d74963
Hui <xuhuizhu369@gmail.com>
2021-01-26 22:49:27 -0800

    track sass changes

--- src/components/LidMenu/LidMenu.js
+++ src/components/LidMenu/LidMenu.js
@@ -1,12 +1,39 @@
 import './_LidMenu.sass'
-import React from 'react'
+import React, { useReducer } from 'react'
+
+import {
+    lidMenuInit,
+    LIDMENU_ACTIONS,
+    lidMenuReducer,
+} from './_lidMenuReducer'
+
+const { COLOR_LID, COLOR_ITEM, COLOR_SEPARATOR, ADD_ITEM } = LIDMENU_ACTIONS
 
 function LidMenu() {
+
+    const [lidMenuState, dispatchLidMenuState] = useReducer(lidMenuReducer, lidMenuInit)
+
 return (
 <>
-<div className='lidMenu_cont'>
+<div className='lidMenu_cont'
+style={{
+    backgroundColor: lidMenuState.lidColor
+}}>
 
+{
+    lidMenuState.lidItems.map((item, idx) => (
 
+        <div className='lidMenu_item'
+        style={{
+            backgroundColor: lidMenuState.itemColor
+        }}
+        key={idx}
+        >
+            {item}
+        </div>
+
+    ))
+}
 
 </div>
 </>
