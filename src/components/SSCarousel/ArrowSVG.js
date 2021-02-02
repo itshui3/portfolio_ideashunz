bbc6465213eb54b32f70a739d7e61f452e5f7b4c
Hui <xuhuizhu369@gmail.com>
2021-02-02 13:36:15 -0800

    svg arrow cases handled!

--- src/components/SSCarousel/ArrowSVG.js
+++ src/components/SSCarousel/ArrowSVG.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const rewind = { transform: 'rotate(-90deg)' }
 
 function ArrowSVG({ side, isEnd }) {
 
+    const [strokeEmphasis, setStrokeEmphasis] = useState(0)
+
 return (
 <>
 <svg className={`sscarousel_side_svg ${side}`}
-style={isEnd? rewind: null}>
+style={isEnd? rewind: null}
+onMouseOver={() => setStrokeEmphasis(1)}
+onMouseOut={() => setStrokeEmphasis(0)} 
+onMouseDown={() => setStrokeEmphasis(2)}
+onMouseUp={() => setStrokeEmphasis(1)}>
 
     <line 
-    x1="100%" y1="0%" stroke="grey" strokeWidth="5"
+    x1="100%" y1="0%" stroke={
+        !strokeEmphasis? 'grey': strokeEmphasis === 2? '#000': '#708090'
+    } 
+    strokeWidth="5" strokeLinecap="round"
     x2="0%" y1="50%"
     />
     <line 
-    x1="100%" y1="50%" stroke="grey" strokeWidth="5"
+    x1="100%" y1="50%" stroke={
+        !strokeEmphasis? 'grey': strokeEmphasis === 2? '#000': '#708090'
+    } 
+    strokeWidth="5" strokeLinecap="round"
     x2="0%" y2="100%"
     />
 
