3d67b149071a6eed5423dce4e02d6e8296a653e0
Hui <xuhuizhu369@gmail.com>
2021-02-02 20:00:27 -0800

    resizes svg to sit more comfortably

--- src/components/SSCarousel/ArrowSVG.js
+++ src/components/SSCarousel/ArrowSVG.js
@@ -10,10 +10,24 @@ return (
 <>
 <svg className={`sscarousel_side_svg ${side}`}
 style={isEnd? rewind: null}
+
 onMouseOver={() => setStrokeEmphasis(1)}
-onMouseOut={() => setStrokeEmphasis(0)} 
+onMouseOut={() => setStrokeEmphasis(0)}
 onMouseDown={() => setStrokeEmphasis(2)}
 onMouseUp={() => setStrokeEmphasis(1)}>
+    {/* 
+    []arrowSVG emphasis case handling notes
+    []emphasis strengths: [0 - 1 - 2]
+
+    onMouseOver - emphasizeHover(1)
+    onMouseOut - deEmphasizeHover
+
+    onMouseDown - emphasizeClick = 2 potential cases after this
+        =>onMouseUp - deEmphasizeClick(1) 
+        = mouseUp must occur while mouse also onHover, therefore (1)
+        =>onMouseOut - deEmphasizeClick(0)
+        = handles resetting emphasis off (2) in case full onClick never occurs
+    */}
 
     <line 
     x1="100%" y1="0%" stroke={
